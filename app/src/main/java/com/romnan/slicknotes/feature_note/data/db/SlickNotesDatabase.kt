3b03c3250517bd4457585d1b15cac9160aab03d7
Deddy Romnan <romnandeddy@gmail.com>
2021-11-24 10:49:41 +0700

    Create SlickNotesDatabase.kt

---
+++ app/src/main/java/com/romnan/slicknotes/feature_note/data/db/SlickNotesDatabase.kt
@@ -0,0 +1,17 @@
+package com.romnan.slicknotes.feature_note.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.romnan.slicknotes.feature_note.data.db.dao.NoteDao
+import com.romnan.slicknotes.feature_note.domain.model.Note
+
+@Database(
+    entities = [Note::class],
+    version = 1
+)
+abstract class SlickNotesDatabase : RoomDatabase() {
+    companion object{
+        const val DATABASE_NAME = "slick_notes_db"
+    }
+    abstract val noteDao: NoteDao
+}
